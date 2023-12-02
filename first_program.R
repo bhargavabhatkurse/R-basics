724ca8a2731316eac40ea031e969a74d42fd208c
Bhargava Bhatkurse <bhargava.bhatkurse@gmail.com>
2023-12-02 21:30:39 +0530

    first commit

---
+++ first_program.R
@@ -0,0 +1,18 @@
+print("Bhargava bhatkurse")
+5+6
+sum <- 37+45
+print(sum)
+demo <- "hello programming"
+
+#this is a comment
+var1 <- 1 ; var2 <- "2"          #semicolon to seperate
+sessionInfo()
+
+#plotting a graph
+x<-1:20
+plot(x)
+
+?plot #to get the help for plot function in Help section
+
+#R is case sensitive language
+a=10;A=10
